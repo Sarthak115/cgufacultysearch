4e72a7d820e0f94c1676065425f51770d970a784
sarthak115 <supersarthak8@gmail.com>
2025-07-01 19:18:42 +0530

    title fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'CGU Faculty Search',
+  description: 'Explore a comprehensive directory of faculty members at C.V. Raman Global University. Easily find department-wise contact details, email addresses, and designations to connect with professors and teaching staff—built to make academic communication faster and smoother for students.',
   generator: 'v0.dev',
 }
 
@@ -14,11 +14,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <title> CGU faculty search </title>
         <meta name="description" content="Explore a comprehensive directory of faculty members at C.V. Raman Global University. Easily find department-wise contact details, email addresses, and designations to connect with professors and teaching staff—built to make academic communication faster and smoother for students." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+      </head>
       <body>{children}
          {/* ✅ Visitor Counter Code */}
         <a href="http://www.freevisitorcounters.com">more information</a>
